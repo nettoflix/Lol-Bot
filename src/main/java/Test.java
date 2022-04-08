a845772360466ac2fc85a3636cce571e3d0a2028
unknown <netto2002.sjb@gmail.com>
2022-04-08 17:04:27 -0300

    Added Lifebars

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,4 @@
+import org.sikuli.basics.Settings;
 import org.sikuli.script.*;
 
 import java.net.URISyntaxException;
@@ -12,44 +13,73 @@ public class Test {
 
     private void start() {
         try{
+
             s = new Screen();
-            //String imagePath = "perfil.png";
+
             URL resourceFolderURL = this.getClass().getClassLoader().getResource("Imagens");
             basePath = resourceFolderURL.toURI().getPath() + "/";
-            String finishedBanditImage = basePath + "finished_tentBanditLeader.png";
-            String sendOnQuestImage = basePath + "sendOnQuest.png";
-            String confirmImage = basePath + "confirm.png";
-            String finishedElvishForestImage = basePath + "finished_elvishForest";
+            String platiniumImage = basePath + "platinium.png";
+            String iconeMorgana_Image = basePath + "iconeMorgana.png";
+            String lifeBar1_Image = basePath + "lifeBar1.png";
+            String[] lifeBars = {"lifeBar1.png", "lifeBar2.png", "lifeBar3.png","lifeBar4.png",
+                    "lifeBar5.png", "lifeBar6.png","lifeBar7.png","lifeBar8.png","lifeBar9.png",
+                    "lifeBar10.png","lifeBar11.png","lifeBar12.png","lifeBar13.png", "lifeBar14.png",
+                    "lifeBar15.png","lifeBar16.png"};
             Region region1 = new Region(416, 231,850,210);
+            Settings.MoveMouseDelay = 0f;
+            Settings.MinSimilarity = 0.5;
+            Pattern pattern = new Pattern();
+
+
             while(true)
             {
-                if(clickOnImage(region1, finishedBanditImage))
+                for(int i=0; i<lifeBars.length; i++)
                 {
-                    System.out.println();
-                    clickOnImage(null, sendOnQuestImage);
-                    sleep(7000);
-                    clickOnImage(null, confirmImage);
+                    try
+                    {
+                        String lifeBarPath = basePath + lifeBars[i];
+                        Match lifeBarFound = s.find(lifeBarPath);
+                        if (lifeBarFound != null) {
+                            System.out.println("Found image with score: " + lifeBarFound.getScore());
+                            //s.find(iconeMorgana_Image).highlight();
+                            if (lifeBarFound.getScore() >= 0.7) {
+                                s.mouseMove(lifeBarPath);
+                                s.mouseMove(-60,60);
+                                break;
+                                //s.wait(iconeMorgana_Image);
+                                //s.mouseMove(iconeMorgana_Image);
+                            }
+                        }
+                    }
+                    catch(FindFailed e){}
+                }
+                //if(clickOnImage(platiniumImage))
+                //{
+                  //  System.out.println("clicked");
+                //}
+               /* try {
+                    //System.out.println("oi");
+                    System.out.println("Find: " + s.find(iconeMorgana_Image).toString());
 
+                    if (s.find(lifeBar1_Image) != null) {
+                        System.out.println("Found image with score: " + s.find(lifeBar1_Image).getScore());
+                        //s.find(iconeMorgana_Image).highlight();
+                        if (s.exists(lifeBar1_Image).getScore() >= 0.7) {
+                            s.mouseMove(lifeBar1_Image);
+                            //s.wait(iconeMorgana_Image);
+                            //s.mouseMove(iconeMorgana_Image);
+                        }
+                    }
                 }
-                sleep(7000);
-                if(clickOnImage(region1,finishedElvishForestImage))
-                {
-                    clickOnImage(null,sendOnQuestImage);
-                    sleep(7000);
-                    clickOnImage(null,confirmImage);
-                }
+                catch(FindFailed e) {
+                   // System.out.println(e.getMessage());
+                } */
             }
 
 
 
-            //String pathYourSystem = System.getProperty("user.dir") + "\\";
-            //s.wait(pathYourSystem+ "perfil.png");
-            //s.click(pathYourSystem+ "perfil.png");
-            //s.wait("imgs/spotlight-input.png");
-
-
         }
-        catch(FindFailed | URISyntaxException | InterruptedException e){
+        catch(/*FindFailed |*/ URISyntaxException /*| InterruptedException*/ e){ //
             e.printStackTrace();
         }
     }
@@ -79,10 +109,24 @@ public class Test {
                     s.click(path);
                     succes = true;
                 }
-
-
             }
         }
+        return succes;
+    }
+    private boolean clickOnImage(String path) throws FindFailed, InterruptedException {
+        boolean succes=false;
+            if(s.exists(path)!=null)
+            {
+                if(s.exists(path).getScore() >=0.8)
+                {
+                    s.wait(path);
+                    s.mouseMove(path);
+                    s.mouseDown(Button.LEFT);
+                    sleep(500);
+                    s.mouseUp(Button.LEFT);
+                    succes = true;
+                }
+            }
         return succes;
     }
 
